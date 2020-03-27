fee8899d89ebe94c2d97378e23599bbe21171a23
liangxulong <liangxulong@xin.com>
2020-03-27 11:03:07 +0800

    fix

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('blog/<int:pk>/', views.detail, name='detail'),
+    path('archives/<int:year>/<int:month>/', views.archives, name='archives'),
+    path('category/<int:category_id>/', views.category, name='category'),
+    path('tag/<int:tag_id>/', views.tag, name='tag'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,36 @@ import re
 
 from django.shortcuts import render, get_object_or_404
 
-from blog.models import Post
+from blog.models import Post, Category, Tag
 
 import markdown
 
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/index.html', context={
+        'post_list': post_list,
+    })
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_at__year=year, created_at__month=month).order_by('-created_at')
+    return render(request, 'blog/index.html', context={
+        'post_list': post_list,
+    })
+
+
+def category(request, category_id):
+    cate = get_object_or_404(Category, pk=category_id)
+    post_list = Post.objects.filter(category=cate).order_by('-created_at')
+    return render(request, 'blog/index.html', context={
+        'post_list': post_list
+    })
+
+
+def tag(request, tag_id):
+    tg = get_object_or_404(Tag, pk=tag_id)
+    post_list = Post.objects.filter(tags=tg).order_by('-created_at')
     return render(request, 'blog/index.html', context={
         'post_list': post_list,
     })
